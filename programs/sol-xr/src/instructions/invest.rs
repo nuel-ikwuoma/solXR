9e98852e04ecd8a860eb36efd7e71941a4899703
precious <owo.pre.eno@gmail.com>
2025-03-24 03:07:32 +0100

    initialize iteration x

---
+++ programs/sol-xr/src/instructions/invest.rs
@@ -0,0 +1,10 @@
+use {
+    anchor_lang::prelude::*,
+    anchor_spl::{
+        associated_token::AssociatedToken,
+        token::{mint_to, Mint, MintTo, Token, TokenAccount},
+    },
+};
+
+#[derive(Accounts)]
+pub struct MintToken<'info>
